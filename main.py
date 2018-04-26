5a87c65732f21ef505dac0caf0fe0975124c0a3c
Alex Vig <jalexvig@gmail.com>
2018-04-26 12:38:42 -0400

    Updated descriptions for flags

--- main.py
+++ main.py
@@ -11,25 +11,27 @@ def parse_flags():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--env', default='binary', help='Name of environment')
-    parser.add_argument('--model_dir', default='./saved')
-    parser.add_argument('--seed', default=3, type=int, help='Random seed')
-    parser.add_argument("--n_iter", default=1000, help="Number iterations", type=int)
-    parser.add_argument('--dont_normalize_advantages', '-dna', action='store_true')
-    parser.add_argument("--mix_halflife", default=10000.0, help="Halflife of gradient/action mixture", type=float)
-    parser.add_argument("--mix_start", default=1.0, help="Starting weight for gradient", type=float)
-    parser.add_argument("--batch_size", default=3, help="Batch size", type=int)
-    parser.add_argument("--discount", default=0.99, help="Discount rate for rewards", type=float)
-    parser.add_argument("--ep_len", default=10, help="Number of steps before performing an update", type=int)
-    parser.add_argument("--num_lstm_units", default=4, help="Number LSTM units", type=int)
-    parser.add_argument("--grad_reg", help="Cap for l2 norm of gradients", type=int)
-    parser.add_argument('--no_xover', action='store_true')
-    parser.add_argument('--render', action='store_true')
-    parser.add_argument("--histogram_parameters", action='store_true')
-    parser.add_argument("--reset", action='store_true',
-                        help="If set, delete the existing model directory and start training from scratch.")
-    parser.add_argument('--debug', action='store_true')
-    parser.add_argument("--run_name", default="default", help="Name of run.")
+    parser.add_argument('--env', default='binary', help='Name of environment.')
+    parser.add_argument('--model_dir', default='./saved', help='Directory to save run information in.')
+    parser.add_argument('--seed', default=3, type=int, help='Random seed.')
+    parser.add_argument('--n_iter', default=1000, type=int, help='Number iterations.')
+    parser.add_argument('--dont_normalize_advantages', '-dna', action='store_true',
+                        help='Advantage normalization can help with stability. Turns that feature off.')
+    parser.add_argument('--mix_halflife', default=10000.0, type=float, help='Halflife of gradient/action mixture.')
+    parser.add_argument('--mix_start', default=1.0, type=float,
+                        help='Starting weight for magnitude of gradient contributions.')
+    parser.add_argument('--batch_size', default=3, type=int, help='Batch size when collecting gradients for policy.')
+    parser.add_argument('--discount', default=0.99, type=float, help='Discount rate for rewards.')
+    parser.add_argument('--ep_len', default=10, type=int, help='Number of steps before performing an update.')
+    parser.add_argument('--num_lstm_units', default=4, type=int, help='Number LSTM units in policy hidden layer.')
+    parser.add_argument('--grad_reg', type=int, help='Cap for l2 norm of gradients.')
+    parser.add_argument('--no_xover', action='store_true', help='Treat each gradient as separate input to policy.')
+    parser.add_argument('--render', action='store_true', help='Not implemented.')
+    parser.add_argument('--histogram_parameters', action='store_true', help='Record histogram of parameters.')
+    parser.add_argument('--reset', action='store_true',
+                        help='Delete the existing model directory and start training from scratch.')
+    parser.add_argument('--debug', action='store_true', help='Run session in a CLI debug session.')
+    parser.add_argument('--run_name', default='default', help='Name of run.')
 
     parser.parse_args(namespace=CONFIG)
 
