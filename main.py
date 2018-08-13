77c19d860759e75e266c70fc6220a4aaa759c2a4
Alex Vig <jalexvig@gmail.com>
2018-08-13 18:21:12 -0400

    Updated defaults

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ def parse_flags():
     parser.add_argument('--n_iter', default=1000, type=int, help='Number iterations.')
     parser.add_argument('--dont_normalize_advantages', '-dna', action='store_true',
                         help='Advantage normalization can help with stability. Turns that feature off.')
-    parser.add_argument('--mix_halflife', default=10000.0, type=float, help='Halflife of gradient/action mixture.')
-    parser.add_argument('--mix_start', default=1.0, type=float,
+    parser.add_argument('--mix_start', default=0.0, type=float,
                         help='Starting weight for magnitude of gradient contributions.')
+    parser.add_argument('--mix_halflife', default=10000.0, type=float, help='Halflife of gradient/action mixture.')
     parser.add_argument('--batch_size', default=3, type=int, help='Batch size when collecting gradients for policy.')
     parser.add_argument('--discount', default=0.99, type=float, help='Discount rate for rewards.')
     parser.add_argument('--ep_len', default=10, type=int, help='Number of steps before performing an update.')
     parser.add_argument('--num_lstm_units', default=3, type=int, help='Number LSTM units in policy hidden layer.')
-    parser.add_argument('--grad_reg', type=int, help='Cap for l2 norm of gradients.')
+    parser.add_argument('--grad_reg', type=float, help='Cap for l2 norm of gradients.')
     parser.add_argument('--no_xover', action='store_true', help='Treat each gradient as separate input to policy.')
     parser.add_argument('--render', action='store_true', help='Not implemented.')
     parser.add_argument('--load_params_torch', help='Load policy parameters from a pytorch model.')
